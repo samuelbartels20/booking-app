7309fbcc58ff3491328ce20f52f509aeb9250abf
samuelbartels20 <bartelssamuel20@gmail.com>
2022-01-01 23:59:42 +0000

    modified:   main.go

--- main.go
+++ main.go
@@ -6,8 +6,10 @@ func main() {
 
 	ConferenceName := "Go Conference"
 	const conferenceTicket = 50
+	remainingTickets := 50
 
 	fmt.Println("Welcome to", ConferenceName , "booking application")
+	fmt.Println("We have total of", remainingTickets, "tickets and", remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
 	
