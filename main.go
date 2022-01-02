f94087d9555e5f0ea7ec5acd6b0d5edadcbffb57
samuelbartels20 <bartelssamuel20@gmail.com>
2022-01-02 00:50:52 +0000

    modified:   main.go

--- main.go
+++ main.go
@@ -8,16 +8,15 @@ func main() {
 	const conferenceTickets = 50
 	remainingTickets := 50
 
-	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
-
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v, are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string
+	var firstName string
 	var userTickets int
+	fmt.Println("Please enter your first name: ")
+	fmt.Scan(&firstName)
 
-	userName = "Tom"
 	userTickets = 2
 	fmt.Printf("user %v booked %v tickets.\n", userName, userTickets)
 
